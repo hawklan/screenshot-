41108a20e8133cd4974a2b93e53fbb5b6267e6c0
Azuvector <azuvector@gmail.com>
2013-01-24 19:16:38 +0000

    Shaping up a bit...
    git-svn-id: https://localhost/svn/private/Screenshot++@957 95f70f3f-bffb-4027-969e-e3704ff02e53

--- Screenshot++/Capture.cs
+++ Screenshot++/Capture.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace Screenshot__
 {
@@ -15,8 +18,7 @@ namespace Screenshot__
             if (nCode >= 0 && wParam == (IntPtr)WindowMessages.WM_SYSKEYDOWN)
             {
                 int vkCode = Marshal.ReadInt32(lParam);
-                //if((Keys)vkCode == 44) // Printscreen
-                if ((Keys)vkCode == Keys.PrintScreen)
+                if ((Keys)vkCode == Keys.PrintScreen) // 44
                 {
                     Console.WriteLine((Keys)vkCode);
                 }
@@ -24,14 +26,87 @@ namespace Screenshot__
             else if (nCode >= 0 && wParam == (IntPtr)WindowMessages.WM_KEYDOWN)
             {
                 int vkCode = Marshal.ReadInt32(lParam);
-                //if((Keys)vkCode == 44) // Printscreen
-                if ((Keys)vkCode == Keys.PrintScreen)
+                if ((Keys)vkCode == Keys.PrintScreen) // 44
                 {
-                    Console.WriteLine((Keys)vkCode);
+                    Rectangle rect = SystemInformation.VirtualScreen;
+                    Bitmap bmp = CaptureRegion(rect.Left, rect.Top, rect.Width, rect.Height);
+
+                    EncoderParameters encParams = new EncoderParameters(1);
+                    encParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, (long)75);
+
+                    bmp.Save(GetNextFileName(AppendTimestamp("Screenshot++"), ".jpg"), GetEncoder(ImageFormat.Jpeg), encParams);
                 }
             }
             
-            return true; // This gets discarded anyway.
+            return true;
         }
+
+        public static string AppendTimestamp(string str)
+        {
+            DateTime date = DateTime.Now;
+            return string.Format("{0} {1}-{2}-{3}_{4}h{5}m{6}s", str, date.Year.ToString("d4"), date.Month.ToString("d2"), date.Day.ToString("d2"), date.Hour, date.Minute, date.Second);
+        }
+
+        public static string GetNextFileName(string baseFilename, string extension, string path = "./")
+        {
+            string combinedFilename = string.Format("{0}{1}", baseFilename, extension);
+            string cPath = Path.Combine(path, combinedFilename);
+            string pPath = cPath;
+
+            for(int count = 2;File.Exists(pPath);count++) // Start at 2 for neatness' sake.
+            {
+                pPath = Path.Combine(path, string.Format("{0} ({1}){2}", Path.GetFileNameWithoutExtension(cPath), count, Path.GetExtension(cPath)));
+            }
+            return pPath;
+        }
+
+        public static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        public static Bitmap CaptureRegion(int x, int y, int w, int h)
+        {
+            IntPtr hDesktop = GetDesktopWindow();
+            IntPtr hSourceDC = GetWindowDC(hDesktop);
+            IntPtr hDestinationDC = CreateCompatibleDC(hSourceDC);
+            IntPtr hBitmap = CreateCompatibleBitmap(hSourceDC, w, h);
+            IntPtr hOldBitmap = SelectObject(hDestinationDC, hBitmap);
+            BitBlt(hDestinationDC, 0, 0, w, h, hSourceDC, x, y, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+            Bitmap bmp = Bitmap.FromHbitmap(hBitmap);
+            SelectObject(hDestinationDC, hOldBitmap);
+            DeleteObject(hBitmap);
+            ReleaseDC(hDesktop, hSourceDC);
+
+            return bmp; // Dispose?
+        }
+
+        [DllImport("gdi32.dll")]
+        static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, CopyPixelOperation rop);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteDC(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteObject(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr ptr);
     }
 }
